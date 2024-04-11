b6fac9c48aec49101d93e237e0e4efe1ca4deb85
Roudranil <nil0auditore78@gmail.com>
2024-04-11 10:19:52 +0530

    [data] changed json format of recipes data

--- src/process_json.py
+++ src/process_json.py
@@ -6,22 +6,23 @@ import utils
 project_root_directory = utils.find_root_directory()
 DATA_DIR = os.path.join(project_root_directory, "data", "recipes")
 
-data = {}
+data = []
 
 for recipe_category in os.listdir(DATA_DIR):
     with open(os.path.join(DATA_DIR, recipe_category), "r") as f:
         recipes = json.load(f)
 
     for recipe_name in recipes.keys():
-        data[recipe_name] = {}
-        data[recipe_name]["url"] = recipes[recipe_name]["url"]
-        data[recipe_name]["text"] = f"{recipe_name}\n" + "\n".join(
+        recipe_data = {}
+        recipe_data["url"] = recipes[recipe_name]["url"]
+        recipe_data["text"] = f"{recipe_name}\n" + "\n".join(
             [
                 (f"{k}\n" + recipes[recipe_name][k])
                 for k in recipes[recipe_name].keys()
                 if k != "url"
             ]
         )
+        data.append(recipe_data)
 
 with open(os.path.join(project_root_directory, "data", "all-recipes.json"), "w") as f:
     json.dump(data, f, indent=4)
